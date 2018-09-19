b89f39aa591fd9fd8a25d8a986d0b6a3270061d3
sansc <sansc@10.216.1.26>
2018-09-19 15:54:07 -0400

    Create class DriveTrainTeleop

--- src/main/java/org/usfirst/frc/team1683/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team1683/robot/Robot.java
@@ -4,7 +4,6 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team1683.robot;
 
 import org.usfirst.frc.team1683.robot.subsystems.DriveTrain;
