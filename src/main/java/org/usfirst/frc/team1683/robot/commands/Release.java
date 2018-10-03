9492d21812636a44f1ac9a001a9fb40691200a2b
rache <rache@10.216.118.92>
2018-10-03 08:17:47 -0400

    Work on it

--- src/main/java/org/usfirst/frc/team1683/robot/commands/Release.java
+++ src/main/java/org/usfirst/frc/team1683/robot/commands/Release.java
@@ -1,10 +1,12 @@
 package org.usfirst.frc.team1683.robot.commands;
 
+import org.usfirst.frc.team1683.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Release extends Command {
 	public void execute() {
-		
+		Robot.grabberRight.set(0.5);
 	}
 
 	@Override
