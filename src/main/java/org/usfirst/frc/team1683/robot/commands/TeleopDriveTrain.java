ee7778bb41feefa9e9a59e84f1a4ca5227e9c4d1
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2018-10-04 22:34:51 -0400

    Added teleop filters

--- src/main/java/org/usfirst/frc/team1683/robot/commands/TeleopDriveTrain.java
+++ src/main/java/org/usfirst/frc/team1683/robot/commands/TeleopDriveTrain.java
@@ -2,8 +2,8 @@ package org.usfirst.frc.team1683.robot.commands;
 
 import org.usfirst.frc.team1683.robot.HWR;
 import org.usfirst.frc.team1683.robot.Robot;
+import org.usfirst.frc.team1683.robot.motor.Filter;
 
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
@@ -13,9 +13,20 @@ public class TeleopDriveTrain extends Command {
 	private Joystick leftJoystick = new Joystick(HWR.LEFT_JOYSTICK);
 	private Joystick rightJoystick = new Joystick(HWR.RIGHT_JOYSTICK);
 	private Joystick auxJoystick = new Joystick(HWR.AUX_JOYSTICK);
+
+	private Filter rightFilter, leftFilter, elevatorFilter;
+	public void initialize() {
+		rightFilter = new Filter(0.01);
+		leftFilter = new Filter(0.01);
+		elevatorFilter = new Filter(0.1);
+	}
+
 	public void execute() {
-			Robot.drive.set(-leftJoystick.getY(), -rightJoystick.getY());
-			Robot.elevator.moveUp(-auxJoystick.getY());
+		rightFilter.update(-rightJoystick.getY());
+		leftFilter.update(-leftJoystick.getY());
+		elevatorFilter.update(-auxJoystick.getY());
+		Robot.drive.set(leftFilter.getValue(), rightFilter.getValue());
+		Robot.elevator.moveUp(elevatorFilter.getValue());
 	}
 
 	Button button1 = new JoystickButton(leftJoystick,1),
@@ -28,9 +39,9 @@ public class TeleopDriveTrain extends Command {
 		button4.whenPressed(new Grab());
 		button5.whenPressed(new Release());
 	}
+
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 }
