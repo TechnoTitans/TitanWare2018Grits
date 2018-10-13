4f8cde4f6c31ed283ac526e1ac8a93c47dfdd129
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2018-10-12 22:57:45 -0400

    OutInShake command

--- src/main/java/org/usfirst/frc/team1683/robot/commands/OutInShake.java
+++ src/main/java/org/usfirst/frc/team1683/robot/commands/OutInShake.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team1683.robot.commands;
 
-public class OutInShake {
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
+public class OutInShake extends CommandGroup {
+    public OutInShake() {
+        addSequential(new Jerk(true));
+        addSequential(new Jerk(false));
+        addSequential(new Grab(), 0.5);
+    }
 }
