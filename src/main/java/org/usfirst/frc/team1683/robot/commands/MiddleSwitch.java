201b15ea125961d652aad1a0c3dd2f259dd532a1
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2018-10-13 09:21:32 -0400

    Middle updates

--- src/main/java/org/usfirst/frc/team1683/robot/commands/MiddleSwitch.java
+++ src/main/java/org/usfirst/frc/team1683/robot/commands/MiddleSwitch.java
@@ -12,12 +12,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class MiddleSwitch extends CommandGroup {
   public MiddleSwitch(boolean goRight) {
     addSequential(new OutInShake());
-    addSequential(new Forward(30, 0.5));
-    addSequential(new Turn(90 * (goRight ? 1 : -1), 0.4));
-    addSequential(new Forward(55, 0.5));
+    addSequential(new Forward(50, 0.5));
+    addSequential(new Turn(45 * (goRight ? 1 : -1), 0.4));
+    addSequential(new Forward(66, 0.5));
     addParallel(new LiftUp(true));
-    addSequential(new Turn(90 * (goRight ? -1 : 1), 0.4));
-    addSequential(new Forward(41, 0.3));
+    addSequential(new Turn(-45 * (goRight ? -1 : 1), 0.4));
+    addSequential(new Forward(58, 0.3));
     addSequential(new Release());
   }
 }
